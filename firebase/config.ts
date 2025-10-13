bde7256be73b939e289c16de8e45f84f5e65ac81
Sebastian Cruz <sebpm.98@gmail.com>
2025-10-13 18:36:59 -0500

    Agrego nueva funcionalidad

--- firebase/config.ts
+++ firebase/config.ts
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAEHyTJjG8XEhVepIuUZ4xEKzqPo77BmjU",
-  authDomain: "zona-creps-app.firebaseapp.com",
-  projectId: "zona-creps-app",
-  storageBucket: "zona-creps-app.firebasestorage.app",
-  messagingSenderId: "849605707573",
-  appId: "1:849605707573:web:f0d695f868b601a7306e7c"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
